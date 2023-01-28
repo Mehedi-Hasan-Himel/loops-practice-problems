adb186b48fbfbbb17d512de8ca938549fd4e82b0
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 22:35:44 +0600

    [ADD] For loop, how for loop works, while vs for loop

---
+++ for.js
@@ -0,0 +1,17 @@
+var rostGiven = 1;
+while (rostGiven <= 7) {
+  console.log(rostGiven);
+  rostGiven = rostGiven + 1;
+}
+
+// for loop.
+for (var rostGiven = 0; rostGiven <= 7; rostGiven++) {
+  console.log("Rost Den Please..!");
+}
+
+// simple version of for loop
+console.log(`Simple for loop.`);
+for(var i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
