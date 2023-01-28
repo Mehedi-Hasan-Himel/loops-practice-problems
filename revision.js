d569cebb9ca60f80632e9717b08ff8c1aeb398e6
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-28 21:40:06 +0600

    [ADD] revision previous lecture

---
+++ revision.js
@@ -0,0 +1,26 @@
+// string type variable
+var bottle = `water`;
+var bottleColor = `yellow`;
+var penColor = `blue`;
+
+// number type variable
+var sunglassPrice = 120;
+
+// boolean type variable
+var isExpensive = true;
+
+var tableItems = [`bottle`, `pen`, `mouse`, `sunglass`, `paper`];
+
+var item3 = tableItems[3];
+tableItems[4] = `notebook`;
+var penIndex = tableItems.indexOf(`pen`);
+// push pop
+
+
+// conditionals
+if (tableItems.length > 4){
+  
+   
+}else {
+   
+}
