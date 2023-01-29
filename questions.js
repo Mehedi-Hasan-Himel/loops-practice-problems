c207e067095acbb21adc4132d2ddd18bd306cdd9
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-29 14:02:53 +0600

    [ADD] questions

---
+++ questions.js
@@ -0,0 +1,75 @@
+/* 
+==============================================
+ Variables
+==============================================
+
+// JavaScript questions for interview .
+   1. What is JavaScript ? 
+   2. How does JavaScript work ?
+   3. What is variable ?
+   4. How to declear a variable ?
+   5. Types of variable ? How can you find type of variable ?
+   6. What is data type ?
+   7. primitive vs non-primitive data type ?
+   8. What is primitive data type ?
+   9. What is non-primitive data type ?
+   10. Naming convention for variable ?
+   11. Math operation in JavaScript ?
+   12. shorthand math operation ?
+   13. --, ++
+   14. ParseInt, ParseFloat ?
+   15. toFixed ?
+*/
+
+/* 
+================================================
+   Array questions for interview. (javascript)
+================================================
+   1. What is array ?
+   2. How to declear an array ?
+   3. What is the purpose of array ?
+   4. Number of element in an array ?
+   5. What is index ?
+   6. Find the value of an element using index ?
+   7. change an element value using index ?
+   8. Get the index of an element by value ?
+   9. What does it mean when you get undefined while gettig the value of an element ?
+   10. How can you add an element in an array at the last position?
+   11. How can you remove the last element from an array ?
+   12. Remove the first element from an array ?
+*/
+
+/*
+================================================
+   Conditional questions for interview. (javascript)
+================================================
+
+1. Meaning of > , < , >= , <= , == , === , != , !==
+2. Meaning of && , ||
+3. let's say you have X amount of money, If you have more than 80,000 then you will buy a MacBook. If more than 60,000 then you will buy Lenovo Yoga. If you ave more than 20,000 then you will buy used laptop. otherwise you will use your phone.  
+
+*/
+
+
+/*
+
+================================================
+   Loop questions for interview. (javascript)
+================================================
+
+1. What is loop ?
+2. What is for loop ?
+3. What is while loop ?
+4. What is the difference between for loop and while loop ?
+5. What is the purpose of loop ?
+6. How to use for loop ?
+7. Display "Ajke amar mon valo nei" for 39 times. (loop)
+8. Display numbers between 58 to 98.  (loop)
+9. Show all even numbers from 412 to 456 
+10. Show all odd numbers from 581 to 623
+11. Declear an array for all the topics that you have learned last few days. Then display all the topics using loop. 
+12. Create an array for all the mobile phones you have used before. Then display all the mobile phones using while loop.
+13. Run a loop from 30 to 86. This loop will stop if the value get higer than 44.
+14. Write the price of the books that you have. Display the prices if the prices is lowe than 200.
+  
+*/
